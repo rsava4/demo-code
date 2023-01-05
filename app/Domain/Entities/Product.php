8b57d0c20826a23b954388e5c3c363733b3d7d2b
Roman <askros.roman@gmail.ru>
2023-01-05 12:13:13 +0300

    first commit

---
+++ app/Domain/Entities/Product.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Domain\Entities;
+
+final class Product{
+	public $id;
+	public $name;
+	public $price;
+	public $attributes;
+	public $id;
+
+	public function __construct()
+	{
+		
+	}
+}
